f7d63d71e3b464a7ec088e330d5ad2f0f00dfaff
Carsen Yates <carsenyates559@gmail.com>
2025-10-16 16:24:54 -0700

    react native slider was added hastily, removed

--- core/mote/usePublish.tsx
+++ core/mote/usePublish.tsx
@@ -2,13 +2,24 @@ import {useCallback, useContext, useState} from "react";
 import {PublisherStateType, GenericPublisherType} from "./types";
 import MoteContext from "@/core/layouts/MoteLayout/MoteContext";
 
-export default function usePublisher<T>(call: GenericPublisherType<T>) {
+
+export type GenericPublishType<T=string | null | undefined> =
+  (topic: string, message: T) => void;
+
+export interface PublisherControllerType<T> extends PublisherStateType<T> {
+  send: GenericPublishType<T>;
+}
+
+export default function usePublisher<T>(call: GenericPublisherType<T>): PublisherControllerType<T> {
   const [state, setState] = useState<PublisherStateType<T>>({});
   const send = useCallback((topic: string, message: T) => {
-    setState(prev => ({...prev, topic, message, response: null}));
+    setState({topic, message, response: null});
+    if(!topic) {
+      return;
+    }
     call(topic, message).then(response => setState(prev => ({...prev, response})));
   }, [call]);
-  return {send, response: state}
+  return {send, ...state};
 }
 
 
